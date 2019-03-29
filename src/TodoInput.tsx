65dade0f553f646d0c1123b0d82aeb2cb9a7cb91
Emile Maras <e.maras@castsoftware.com>
2019-03-29 18:10:36 +0100

    styling try

--- src/TodoInput.tsx
+++ src/TodoInput.tsx
@@ -10,25 +10,35 @@ interface IProps {
 }
 
 const TodoInput: React.SFC<IProps> = props => (
-    <div>
-    <label>Tranche</label>
-    <input
-      type="number"
-      step="10000"
-      value={props.tranche}
-      onChange={e => props.handleChange(e.target.valueAsNumber)}
-    /> 
-    <label>Taux</label>
-    <input
-      type="number"
-      min="0"
-      max="100"
-      step="5"
-      value={props.taux}
-      onChange={e => props.handleChangeTaux(e.target.valueAsNumber)}
-    />                 
-    <button onClick={props.handleSubmit}>Ajouter</button>
-    <div>{props.errormessage}</div>
+    <div className="half-container">
+     
+            
+        <div className="flex-row">
+            <div className="flex-large">
+            <label>Limite haute de la tranche (€)</label>
+            <input
+              type="number"
+              step="10000"
+              value={props.tranche}
+              onChange={e => props.handleChange(e.target.valueAsNumber)}
+            />
+            </div>
+            <div className="flex-large">
+            <label>Taux</label>
+            <input
+              type="number"
+              min="0"
+              max="100"
+              step="5"
+              value={props.taux}
+              onChange={e => props.handleChangeTaux(e.target.valueAsNumber)}
+            />
+            </div>
+        </div>    
+        <div className="text-center">            
+            <button onClick={props.handleSubmit}>Ajouter</button>
+        </div>
+        <div>{props.errormessage}</div>
     </div>
     
 
