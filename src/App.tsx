998ce0f102705aa4a13641032b3157d5fcc7af27
Emile Maras <e.maras@castsoftware.com>
2019-03-29 19:18:34 +0100

    add dumb figure

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import * as React from 'react';
 
 import TodoListContainer from './TodoListContainer';
 import TodoInputContainer from './TodoInputContainer';
+import Figure from './Figure';
 
 export default () => (
   <div>
     <TodoInputContainer />
     <TodoListContainer />
+    <Figure />
   </div>
 );
