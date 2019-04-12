2adea247598ba10406052df3df7ac5066fc352e9
e1000_3000 <e1000.3000@gmail.com>
2019-04-12 20:44:31 +0200

    Figure heritage direct

--- src/Figure.tsx
+++ src/Figure.tsx
@@ -9,12 +9,39 @@ class Figure extends React.Component<ITodos , {}> {
 
       
   public render() {
-    var xvalue: number[] = [];
-    var yvalue: number[] = [];
-    for (let todo of this.props.todoData){
-        xvalue.push(todo.tranche)
-        yvalue.push(todo.taux)
+    var xvalue: number[] = [0];
+    var yvalue: number[] = [0];
+    var contribution: number = 0;
+    var itranche: number = 0;
+    var trancheNext: number;
+    var xlast: number, tranche: number;
+    const xMax: number = 2000000;
+    const taux: Array<number> = this.props.todoData.taux;
+    const tranches: Array<number> = this.props.todoData.tranche;
+    tranches.push(10000000000000)
+  while (xvalue[xvalue.length-1] < xMax){
+        if (xvalue[xvalue.length-1]>1000000) {
+            xvalue.push(xvalue[xvalue.length-1]+1000)
+        }
+        else {
+            xvalue.push(xvalue[xvalue.length-1]*1.02) 
+        }
     }
+    itranche = 0;
+    contribution = 0;
+    xlast=xvalue[xvalue.length-1];
+    for (let t of taux){
+        tranche = tranches[itranche]
+        trancheNext = tranches[itranche+1]
+        if (xlast < t){
+            break
+        }
+        else {
+            contribution += (Math.min(xlast,trancheNext) - tranche)*t/100
+            
+        }
+    }
+    yvalue.push(xlast-contribution)
     return (
       <Plot
         data={[
