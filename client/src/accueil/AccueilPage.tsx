c1490287d42f651abdb09f3a65e7c53d96c321d7
Emile Maras <e.maras@castsoftware.com>
2019-09-01 17:47:19 +0200

    Donne les défénitions

--- client/src/accueil/AccueilPage.tsx
+++ client/src/accueil/AccueilPage.tsx
@@ -8,9 +8,36 @@ class AccueilPage extends React.Component {
         return(
          <div>
             <h3>
-            Et si on mutualisait une partie de l'héritage?
+            Repensons (les modes de transmission de) l'héritage?
             </h3>
-blablablabla                
+            <h4>
+            Définitions    
+            </h4>
+<ul>
+ <li>                
+  Les <b>modes de transmission de l'héritage</b> sont les moyens qui permettent de transférer du patrimoine d'une personne à une autre sans contrepartie. 
+  La grande majorité de l'héritage est actuellement transmis en ligne direct (i.e. entre parent et enfants).
+  En France, il y a deux modes principaux de transmission de l'héritage, la <b>donation</b> et la <b>succession</b>. 
+ </li>               
+ <li>                 
+  La <b>succession</b> est une transmission du patrimoine d’une personne décédée à une ou plusieurs personnes vivantes.
+ </li>               
+ <li>                 
+
+  La <b>donation</b> est une transmission d’une partie du patrimoine d’une personne vivante à une ou plusieurs personnes.
+ </li>
+ <li>
+  Le patrimoine d’une personne est constitué de l’ensemble de ses biens et de ses obligations, appréciables en argent, et dans lequel entrent les actifs (valeurs, créances) et les passifs (dettes, engagements). 
+ </li>           
+</ul>
+            <h4>
+            Pourquoi repenser les modes de transmission de l'héritage?
+            </h4>
+Il est nécessaire de repenser et de transformer les modes de transmission de l'héritage car :
+<ul>
+  <li> l'héritage représente en moyenne 20 % du revenu total des français </li>
+  <li> l'héritage est très inégalement redistribué </li>  
+</ul>
 
          </div>
         )
