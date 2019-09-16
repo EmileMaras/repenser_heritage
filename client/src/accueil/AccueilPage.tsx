23976f98d1c6d9eb449718e324dadc6a3db00385
Emile Maras <e.maras@castsoftware.com>
2019-09-16 08:45:11 +0200

    add link to simulateur

--- client/src/accueil/AccueilPage.tsx
+++ client/src/accueil/AccueilPage.tsx
@@ -3,6 +3,7 @@ import {StateConsumer, initialTodos} from '../StateContainer'
 import FigureDistributionBrute from './FigureDistributionBrute'
 import FigureDistribution from './FigureMutualisee'
 import axios from "axios";
+import { NavLink} from "react-router-dom";
 
 class AccueilPage extends React.Component {
   viewHandler = async () => {
@@ -110,21 +111,21 @@ De plus plusieurs facteurs amplifient ces inégalités :
             </h4>
 Il est possible de réformer les modes de transmission de l'héritage afin de diminuer les inégalités.
 On pourrait par exemple mutualiser une partie de l'héritage selon les principes suivants :
-à chaque fois qu'une personne bénéficie d'un héritage par donnation ou succession, une partie de cet héritage est mutualisée afin d'être redistribué équitablement au sein de la population. 
+à chaque fois qu'une personne bénéficie d'un héritage par donnation ou succession, une partie de cet héritage est mutualisé afin d'être redistribué équitablement au sein de la population. 
 Le taux de mutualisation dépend uniquement de la valeur cumulée de l'ensemble des héritages dont cette personne a bénéficiée et augmente progressivement par tranche.   
 
 L'argent ainsi récolté alimente la caisse nationale des héritages. A la fin de chaque année fiscale, l'argent accumulé serait divisé en parts et redistribué. 
 Le nombre de parts distribuées serait proportionnel au nombre de décès. 
 Chaque citoyen pourrait à partir de sa majorité demander quand il le souhaite à toucher sa part d'héritage. Il ne pourrait toucher qu'une seule part au court de sa vie. Il pourrait demander à toucher sa part intégralement en une fois ou il pourrait demander à la toucher en plusieurs fois.
 
-A la mise en place d'une telle réforme, la majorité des citoyens n'aurait pas encore touché leur part d'héritage. 
+A la mise en place d'une telle réforme, la majorité des citoyens n'ont pas encore touché leur part d'héritage. 
 Il y aurait donc initialement bien plus de demandeurs que de parts disponibles. 
-Les demandeurs sont sélectionnés en commencant par les plus âgés jusqu'à ce que toutes les parts disponibles soient distribuée.
-Les demandeux restant ne peuvent pas toucher leur part d'héritage cette année là et doivent renouveler leur demande ultérieurement.
+Les demandeurs sont sélectionnés en commencant par les plus âgés jusqu'à ce que toutes les parts disponibles soient distribuées.
+Les demandeux restants ne peuvent pas toucher leur part d'héritage cette année là et doivent renouveler leur demande ultérieurement.
         
 Afin de faire dimininuer progressivement l'âge moyen auquel les gens peuvent toucher leur part d'héritage mutualisé, 
 le nombre de parts distribuées serait initialement suppérieur au nombre de décès. 
-On pourrait par exemple fixer le nombre de parts à 1,5 fois le nombre de décès ce qui permet de faire dimininuer chaque année 
+On peut par exemple fixer le nombre de parts à 1,5 fois le nombre de décès ce qui permet de faire dimininuer chaque année 
         d'environ 6 mois l'âge moyen auquel les citoyens peuvent toucher leur part d'héritage mutualisé 
 
 A long terme, pour une population stable, il y aurait en moyenne autant de demandeurs que de décès et il y aurait autant de parts distribuées que de demandeurs.
@@ -165,8 +166,8 @@ Afin d'illustrer le principe, je propose les taux suivants qui selon mon opinion
         </tbody>
       </table>
     </div>
-Je précise également que cette contribution à la mutualisation remplacerait les taxes sur les successions et donations qui existent actuellement. 
-Afin de ne pas faire de trou dans le budget de l'état, un montant équivalent à ce qui est acutuellement perçu sur les taxes sur les donations et succession serait prélevé par l'état dans la caisse nationale des héritages.
+Je précise également que cette contribution à la mutualisation remplace les taxes sur les successions et donations qui existent actuellement. 
+Afin de ne pas faire de trou dans le budget de l'état, un montant équivalent à ce qui est acutuellement perçu sur les taxes sur les donations et succession est prélevé par l'état dans la caisse nationale des héritages.
 
 La figure suivante présente la distribution actuelle de l'héritage (rouge) ainsi que celle résultant d'une telle réforme (bleu).
 Avec les taux de mutualisation proposés, l'héritage mutualisé représenterait environ 50% de l'héritage totale.
@@ -177,9 +178,22 @@ Cette réforme serait bénéficiaire pour plus de 80% des citoyens (puisque la v
 
         
 Les taux de mutualisation devraient être fixés après avoir été débatus au sein de la société. 
-         </div>
+Afin que chacun puisse se faire sa propre idée sur les taux de mutualisation qui lui semble juste, 
+un simulateur vous permet de voir quelle serait la distribution de l'héritage en fonction des taux de mutualisation que vous aurez choisis :
+        
+<nav>
+    <NavLink
+      to="/simulation"
+      //className="header-link"
+      //activeClassName="header-link-active"
+    >
+    Lien vers le simulateur
+    </NavLink>
+</nav>
+         </div>        
+
         )
     }
 }
-
+         
 export default AccueilPage;
